e75d5fe0bb5c26a3a73769a71f7d7d781d6b2e28
Sagar Timalsina <sgrtimalsina7@gmail.com>
2017-07-19 13:12:11 +0545

    Services added

--- src/app/activity-list/activity-list.component.ts
+++ src/app/activity-list/activity-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {IActivity} from '../shared/activity.model';
+import {ActivityService} from '../services/activity.service';
 
 @Component({
   selector: 'app-activity-list',
@@ -7,7 +9,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ActivityListComponent implements OnInit {
 
-  constructor() { }
+  activities: IActivity[];
+  constructor(private _activityService: ActivityService ) { }
+
 
   ngOnInit() {
   }
---
+++ src/app/shared/activities.ts
@@ -0,0 +1,30 @@
+import { IActivity } from './activity.model';
+
+export const SAVED_ACTIVITIES: IActivity[] = [
+  {
+    'id' : 1,
+    'name' : 'Main Bike Trails',
+    'date' : new Date('06/01/2017'),
+    'distance' : 16.2,
+    'comments' : 'Nice day, cool temps',
+    'gpxData': '../../assets/gpx/1.gpx'
+  },
+  {
+    'id': 2,
+    'name': 'Industrial Park',
+    'date': new Date('06/04/2017'),
+    'gpxData': '../../assets/gpx/2.gpx',
+    'distance': 1.2,
+    'comments': 'Cool and windy.'
+  },
+  {
+    'id': 3,
+    'name': 'Forest Route',
+    'date': new Date('06/05/2017'),
+    'gpxData': '../../assets/gpx/3.gpx',
+    'distance': 3.2,
+    'comments': 'Evening run.'
+  },
+
+]
+
