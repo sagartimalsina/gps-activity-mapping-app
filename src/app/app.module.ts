6ec52189503a01cab4abda01511f2bd7597fff76
Sagar Timalsina <sgrtimalsina7@gmail.com>
2017-07-19 11:37:20 +0545

    Added two components activity-list and map as per the business rules.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ActivityListComponent } from './activity-list/activity-list.component';
+import { MapComponent } from './map/map.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ActivityListComponent,
+    MapComponent
   ],
   imports: [
     BrowserModule
